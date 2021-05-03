c8bf8f2c3b71981203c7e61feea4f50990ead2d6
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-02 21:20:31 -0700

    linking

--- employee-app/src/App.js
+++ employee-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Card from './components/Card'
 
 export default function App() {
     return(
         <Router>
             <div>
                 <Navbar />
+                <Card />
             </div>
         </Router>
     )
