2c72c96d766d9406766f93b916cf12758edec7ee
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-02 21:20:20 -0700

    html elements in jsx

---
+++ employee-app/src/components/Card/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Card(props) {
+    return(
+        <div className="card">
+            <p>This is a card</p>
+
+        </div>
+    )
+}
---
+++ employee-app/src/components/Container/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Container(props) {
+    return(
+        <div 
+        className='container' 
+        style= {props.style}
+        >
+            {props.children}
+        </div>
+    )
+}
