11c78e536027539d4201552eb7751dbeceed8943
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-07 12:27:01 -0700

    navbar with link to search

--- employee-app/src/components/Navbar/index.js
+++ employee-app/src/components/Navbar/index.js
@@ -3,8 +3,20 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return(
-        <nav className='navbar'>
-            <p>This is the navBar</p>
-        </nav>
-    )
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div>
+        <ul className="navbar-nav">
+
+          <li className="nav-item">
+            <Link
+              to="/search"
+              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+            >
+              Search
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
