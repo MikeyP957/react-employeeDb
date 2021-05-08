57e3bc45bb78d0fd1efe163dc6a8233cc585848d
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-07 18:49:12 -0700

    import necessary pages and components to app.js

--- employee-app/src/App.js
+++ employee-app/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Card from './components/Card'
+import Wrapper from './components/Wrapper'
+import Search from './pages/Search'
+import Filter from './pages/Filter'
 
 import EmployeeCard from './components/EmployeeCard';
 
@@ -9,8 +12,12 @@ export default function App() {
     return(
         <Router>
             <div>
+                <Wrapper>
                 <Navbar />
+                <Route exact path="/search" component={Search} />
+                <Route exact path="/filter" component={Filter} />
                 <EmployeeCard />
+                </Wrapper>
 
             </div>
         </Router>
